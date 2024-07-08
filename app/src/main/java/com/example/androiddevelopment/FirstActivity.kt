d1dc135545f90ffc00f47d08d4a9871ada864455
mjmkhandyana <112680514+mjmkhandyana@users.noreply.github.com>
2024-07-08 17:41:07 +0530

    Intents : - implicit intents - explicit intents   -> how to pass data between activities   -> how to pass objects between activities

---
+++ app/src/main/java/com/example/androiddevelopment/FirstActivity.kt
@@ -0,0 +1,36 @@
+package com.example.androiddevelopment
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import org.parceler.Parcels
+
+class FirstActivity : AppCompatActivity() {
+    lateinit var textView: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_first)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        textView = findViewById(R.id.textView)
+
+        // passDataBetweenActivities
+        /*val data = intent.getStringExtra("name")
+        textView.text = data*/
+
+        // passDataBetweenActivitiesUsingSerializable
+        /*val data = intent.getSerializableExtra("person") as User
+        textView.text = data.name*/
+
+        // passDataBetweenActivitiesUsingParcelizeAndParcelable
+        val data = intent.getParcelableExtra<User>("person")
+        textView.text = data?.name
+    }
+}
---
+++ app/src/main/java/com/example/androiddevelopment/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.androiddevelopment
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.os.Parcelable
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlinx.parcelize.Parcelize
+import org.parceler.Parcel
+import org.parceler.Parcels
+
+/*data class User(
+    val name: String
+) :Serializable*/
+
+@Parcelize
+data class User(
+    val name: String
+) : Parcelable
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        // implicit intents
+        val button : Button = findViewById(R.id.webSiteBtn)
+        button.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://www.example.com")
+            startActivity(intent)
+        }
+    }
+
+    fun navigateToAnotherActivityUsingIntents(view: View) {
+        val intent = Intent(this, FirstActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun passDataBetweenActivities(view: View) {
+        val intent = Intent(this, FirstActivity::class.java)
+        intent.putExtra("name", "Madhuri")
+        startActivity(intent)
+    }
+
+    /*fun passObjectsBetweenActivitiesUsingSerializable(view: View) {
+        val intent = Intent(this, FirstActivity::class.java)
+        intent.putExtra("person", User("Madhuri"))
+        startActivity(intent)
+    }*/
+
+    fun passDataBetweenActivitiesUsingParcelizeAndParcelable(view: View) {
+        val intent = Intent(this, FirstActivity::class.java)
+        intent.putExtra("person", User("Madhuri"))
+        startActivity(intent)
+    }
+
+
+}
+
