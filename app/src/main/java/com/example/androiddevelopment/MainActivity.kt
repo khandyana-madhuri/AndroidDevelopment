32e1d6ba7d93c589b5c304658674a8e1cec0e37d
mjmkhandyana <112680514+mjmkhandyana@users.noreply.github.com>
2024-07-09 12:20:07 +0530

    layouts : - Linear Layout - Relative Layout - Frame Layout - Constraint Layout

--- app/src/main/java/com/example/androiddevelopment/MainActivity.kt
+++ app/src/main/java/com/example/androiddevelopment/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.androiddevelopment
 
-import android.content.Intent
-import android.net.Uri
+
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
