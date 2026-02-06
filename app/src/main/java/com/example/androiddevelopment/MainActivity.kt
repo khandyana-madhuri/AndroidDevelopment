821a72a3c4510e446e0a8e9749532d775340ebba
mjmkhandyana <112680514+mjmkhandyana@users.noreply.github.com>
2026-02-06 16:33:18 +0530

    News App: Show the articles in the UI.

--- app/src/main/java/com/example/androiddevelopment/MainActivity.kt
+++ app/src/main/java/com/example/androiddevelopment/MainActivity.kt
@@ -1,13 +1,32 @@
 package com.example.androiddevelopment
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.androiddevelopment.data.remote.dto.ArticleDto
+import com.example.androiddevelopment.ui.viewmodel.NewsUiState
 import com.example.androiddevelopment.ui.viewmodel.NewsViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,8 +39,58 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         setContent {
-           val vm: NewsViewModel = hiltViewModel()
-            Text("Fetching News.. Check Logcat")
+            val vm: NewsViewModel = hiltViewModel()
+            val uiState by vm.uiState.collectAsState()
+            NewsScreen(uiState)
         }
     }
 }
+
+@Composable
+fun NewsScreen(uiState: NewsUiState) {
+    when(uiState) {
+        is NewsUiState.Loading -> {
+            LoadingScreen()
+        }
+
+        is NewsUiState.Success -> {
+            NewsListScreen(uiState.articles)
+        }
+
+        is NewsUiState.Error -> {
+            Toast.makeText(LocalContext.current, uiState.message, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
+
+@Composable
+fun LoadingScreen() {
+    Box(
+      modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
+fun NewsListScreen(articles: List<ArticleDto>) {
+    LazyColumn {
+        items(articles) { article ->
+            Column(modifier= Modifier.padding(16.dp)) {
+                Text(
+                    text = article.title ?: "No Title",
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Text(
+                    text = article.description ?: "No Description",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+    }
+}
+
--- app/src/main/java/com/example/androiddevelopment/ui/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/example/androiddevelopment/ui/viewmodel/NewsViewModel.kt
@@ -3,20 +3,38 @@ package com.example.androiddevelopment.ui.viewmodel
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.androiddevelopment.data.remote.dto.ArticleDto
 import com.example.androiddevelopment.data.repository.NewsRepositoryImpl
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+sealed class NewsUiState {
+    object Loading: NewsUiState()
+    data class Success(val articles: List<ArticleDto>) : NewsUiState()
+    data class Error(val message: String): NewsUiState()
+}
+
 @HiltViewModel
 class NewsViewModel @Inject constructor(val newsRepositoryImpl: NewsRepositoryImpl): ViewModel() {
+
+    private val _uiState = MutableStateFlow<NewsUiState>(NewsUiState.Loading)
+    val uiState: StateFlow<NewsUiState> = _uiState
+
     init {
+        fetchNews()
+    }
+
+    fun fetchNews() {
         viewModelScope.launch {
+            _uiState.value = NewsUiState.Loading
             try {
-                val news = newsRepositoryImpl.fetchNews()
-                Log.d("NEWS_API", "Articles: ${news.size}")
+                val articles = newsRepositoryImpl.fetchNews()
+                _uiState.value = NewsUiState.Success(articles)
             } catch(e: Exception) {
-                Log.d("NEWS_API", "error", e)
+                _uiState.value = NewsUiState.Error(e.localizedMessage ?: "Something Went Wrong")
             }
         }
     }
